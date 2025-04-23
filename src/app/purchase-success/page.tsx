241484253e941380d9994944edbc082e712dd6a8
Lo <lrem@live.fr>
2025-04-23 15:08:58 +0200

    s

--- src/app/purchase-success/page.tsx
+++ src/app/purchase-success/page.tsx
@@ -1,11 +1,18 @@
 import BaseLayout from "@/components/BaseLayout";
 import PurchaseSummary from "./PurchaseSummary";
-
+import { Suspense } from 'react';
 const Page = () => {
 	return (
-		<BaseLayout>
-			<PurchaseSummary />
-		</BaseLayout>
+		<Suspense fallback={<div>Loading...</div>}>
+			<BaseLayout>
+				<PurchaseSummary />
+			</BaseLayout>
+		</Suspense>
+
 	);
 };
 export default Page;
+
+
+
+
